c5a955ba0e81baad807518427565b9414d543162
DavidMoralesz4 <jd3285201@gmail.com>
2024-04-27 02:25:38 -0500

    AppTask

--- src/components/button/CreateTodoButton.jsx
+++ src/components/button/CreateTodoButton.jsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import "./styleButton.css";
 import { TodoContext } from "../../context/TodoProvider";
+import { IoMdAdd } from "react-icons/io";
 
+
+// const add = './public/add.png'
 const CreateTodoButton = () => {
   
   const {setOpenModal} = useContext(TodoContext)
   
   return (
     <>
-      <span onClick={() => setOpenModal(true)}>
-        <img src="./public/add.png" alt="addButton" className="add" />
+      <span onClick={() => setOpenModal(true)} >
+
+      <IoMdAdd className="add"/>
       </span>
     </>
   );
